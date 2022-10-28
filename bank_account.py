6f38502159346e138ba7e53869736bdf9de735c4
Brooke Finister <brookeafinister@gmail.com>
2022-10-28 15:04:31 -0700

    added deposit function

--- bank_account.py
+++ bank_account.py
@@ -1,11 +1,20 @@
 class BankAccount:
     def __init__(self, full_name, account_number, balance):
        self.full_name = full_name
-       self.account_number =account_number
+       self.account_number = account_number
        self.balance = balance
+''' Defined a class BankAccount with an init function that
+takes in 3 parameters, fullname, account_number and balance'''
 
-
+#Deposit Funciton
+def deposit(self):
+   amount = float(input(""))
+   self.balance += amount
+   print("")
+''' Defined a deposit method with the parameter amount
+that allows user to input a float and will add the amount 
+to the balance and print a message "Amount deposited: $x.xx
+New balance: $y.yy "
+'''    
       
-  
-
 
